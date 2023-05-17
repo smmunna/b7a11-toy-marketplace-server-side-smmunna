d28227d006646366258f4b33d6fd2cf17fd327c5
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-17 22:01:17 +0600

    toy router added

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express()
 require('dotenv').config()
 require('./database/db')
 const cors = require('cors');
+const toyRouter = require('./routers/toyRouter')
 const port = process.env.PORT || 5000
 
 // Adding Middlewares
 app.use(cors())
 app.use(express.json())
+
+// Routings for toy's
+app.use('/toys',toyRouter)
 
 app.get('/' , (req , res)=>{
    res.send('Toy Market Place server is successfully running..')
