4c9bfe8ef6488223257ccde433f4b9b553469a01
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-17 22:43:47 +0600

    try catch added

--- routers/toyRouter.js
+++ routers/toyRouter.js
@@ -3,13 +3,19 @@ require('../database/db')
 const toysModel = require('../models/toySchema')
 
 // Toy Information inserting to the MongoDB
-router.post('/',async(req,res)=>{
-    const toysInfo = toysModel(req.body)
-    const result = await toysInfo.save()
-    res.send(result)
+router.post('/', async (req, res) => {
+    try {
+        const toysInfo = toysModel(req.body)
+        const result = await toysInfo.save()
+        res.send(result)
+    } catch (error) {
+        res.send({
+            message:error.message
+        })
+    }
 })
 
-router.get('/',async(req,res)=>{
+router.get('/', async (req, res) => {
     res.send('I am grom toyRouter')
 })
 
